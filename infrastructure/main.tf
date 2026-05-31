9463569216865da98ce11792a04d351cf7a60cf0
mo1mohammad <mo_mohammad1@outlook.de>
2026-05-31 22:59:33 +0200

    Create main.tf

---
+++ infrastructure/main.tf
@@ -0,0 +1,49 @@
+# Provider - sagt Terraform dass wir AWS benutzen
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Security Group - unsere Firewall
+resource "aws_security_group" "grocery_sg" {
+  name        = "grocery-security-group"
+  description = "Security Group fuer die Grocery App"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instanz - unser Server
+resource "aws_instance" "grocery_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.grocery_sg.id]
+
+  tags = {
+    Name = "grocery-server"
+  }
+}
+
+# RDS Datenbank
+resource "aws_db_instance" "grocery_db" {
+  engine         = "mysql"
+  instance_class = "db.t3.micro"
+  username       = "admin"
+  password       = "password123"
+  allocated_storage = 20
+
+  tags = {
+    Name = "grocery-database"
+  }
+}
